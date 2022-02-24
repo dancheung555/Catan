d8876c9c4b57353c70eca446d8dbaeaad26c5ab2
AnDeReturns <69031294+AnDeReturns@users.noreply.github.com>
2022-02-24 13:20:36 -0600

    Aneesh's comment

--- src/main.java
+++ src/main.java
@@ -5,7 +5,6 @@ public class main {
         System.out.println("Hi2");
         System.out.println("From Daniel: Nice, I got it to work at home");
         System.out.println("From Aneesh: Nice, I got it to work at home too!!!");
-
         System.out.println("From Kale: testing updates");
     }
 }
