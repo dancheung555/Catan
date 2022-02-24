a97c6aa27995366fc67c0c34cca29908323858ba
Daniel C <dancheung555@gmail.com>
2022-02-24 13:05:57 -0600

    Test 2

--- src/main.java
+++ src/main.java
@@ -5,5 +5,6 @@ public class main {
         System.out.println("Hi2");
         System.out.println("From Daniel: Nice, I got it to work at home");
         System.out.println("From Aneesh: Nice, I got it to work at home too!!!");
+        System.out.println("Alr it works again at school");
     }
 }
