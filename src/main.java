6dc0af57b4acfc34b8d80c3096daff7c1d635977
Daniel <dancheung555@gmail.com>
2022-02-23 18:33:51 -0600

    Test at home

--- src/main.java
+++ src/main.java
@@ -3,5 +3,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Hi2");
+        System.out.println("From Daniel: Nice, I got it to work at home");
     }
 }
