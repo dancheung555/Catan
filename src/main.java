42b7b46c492ff0ef0cda787c81e3d40c46aed470
Kale <burrito246897531@gmail.com>
2022-04-05 13:08:16 -0500

    test update

--- src/main.java
+++ src/main.java
@@ -7,12 +7,15 @@ public class main {
 
 
     public static void main(String[] args) {
-        mainFrame main = new mainFrame("Catan");
+        //mainFrame main = new mainFrame("Catan");
 
         int roll = 0;
         TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
         for (int i = 0; i < 10000; i++) {
             roll = rollDie();
+            if (!map.containsKey(roll)) {
+                map.put(roll, 0);
+            }
             map.put(roll, map.get(roll) + 1);
         }
 
